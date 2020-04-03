1e033dcc1958201661ccee32a50f9d15490630fd
Steven Wang <steven_wang70@hotmail.com>
2020-04-03 15:09:13 -0400

    Now it really works except could not print out to console.

--- cert/readCerts/readCerts/readCerts.cpp
+++ cert/readCerts/readCerts/readCerts.cpp
@@ -4,6 +4,8 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <sstream>
+#include <iostream>
 
 #include "openssl/bio.h"
 #include <openssl/crypto.h>
@@ -35,28 +37,49 @@ BIO *bio_err = NULL;
 BIO *bio_out = NULL;
 const EVP_CIPHER *enc;
 
-int WORKAROUND_printf(BIO *out, const char *format, ...)
+std::stringstream outs;
+std::stringstream errs;
+
+int WORKAROUND_BIO_printf(BIO *out, const char *format, ...)
 {
+	char buffer[1000];
+
 	va_list args;
 	va_start(args, format);
-//	int ret = BIO_vprintf(out, format, args);
-	int ret = vfprintf((out == bio_err ? stderr : stdout), format, args);
+	vsprintf_s(buffer, format, args);
 	va_end(args);
 
-	return ret;
+	if (out == bio_out)
+	{
+		outs << buffer;
+	}
+	else
+	{
+		errs << buffer;
+	}
+
+	return 0;
 }
 
-int WORKAROUND_puts(BIO *out, const char *buffer)
+int WORKAROUND_BIO_puts(BIO *out, const char *buffer)
 {
-//	return BIO_puts(out, buffer);
-	return fputs(buffer, (out == bio_err ? stderr : stdout));
+	if (out == bio_out)
+	{
+		outs << buffer;
+	}
+	else
+	{
+		errs << buffer;
+	}
+
+	return 0;
 }
 
 int main()
 {
 	const char *infile = "D:\\steven\\gitsrc\\isara\\cert\\certificate.pfx";
 	char password[80] = "123456";
-	int options = 0;
+	int options = 0; // -1 & ~NOCERTS & ~NOKEYS & CACERTS & CLCERTS;
 
 	bio_err = BIO_new_fp(stderr, BIO_NOCLOSE);
 	bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
@@ -67,7 +90,7 @@ int main()
 
     BIO *in = BIO_new_file(infile, "rb");
     if (!in) {
-        WORKAROUND_printf(bio_err, "Error opening input file %s\n",
+        WORKAROUND_BIO_printf(bio_err, "Error opening input file %s\n",
                    infile ? infile : "<stdin>");
         perror(infile);
         return 1;
@@ -79,30 +102,31 @@ int main()
 		return 2;
     }
 
-    if (!password && EVP_read_pw_string(password, sizeof(password), "Enter Import Password:",
+/*    if (!password && EVP_read_pw_string(password, sizeof(password), "Enter Import Password:",
                               0)) {
-        WORKAROUND_printf(bio_err, "Can't read Password\n");
+        WORKAROUND_BIO_printf(bio_err, "Can't read Password\n");
 		return 3;
     }
-
+*/
 	/* If we enter empty password try no password first */
 	if (!password[0] && PKCS12_verify_mac(p12, NULL, 0)) {
 	} else if (!PKCS12_verify_mac(p12, password, -1)) {
-		WORKAROUND_printf(bio_err, "Mac verify error: invalid password?\n");
+		WORKAROUND_BIO_printf(bio_err, "Mac verify error: invalid password?\n");
 		ERR_print_errors(bio_err);
 		return 4;
 	}
-	WORKAROUND_printf(bio_err, "MAC verified OK\n");
+	WORKAROUND_BIO_printf(bio_err, "MAC verified OK\n");
 
     if (!dump_certs_keys_p12(bio_out, p12, password, -1, options, password)) {
-        WORKAROUND_printf(bio_err, "Error outputting keys and certificates\n");
+        WORKAROUND_BIO_printf(bio_err, "Error outputting keys and certificates\n");
         ERR_print_errors(bio_err);
 		return 5;
     }
 
 
-	fflush(stdout);
+//	fflush(stdout);
     BIO_free(in);
+
     return 0;
 }
 
@@ -117,16 +141,17 @@ int dump_certs_keys_p12(BIO *out, PKCS12 *p12, char *pass, int passlen, int opti
 
     if (!(asafes = PKCS12_unpack_authsafes(p12)))
         return 0;
+	int num = sk_PKCS7_num(asafes);
     for (i = 0; i < sk_PKCS7_num(asafes); i++) {
         p7 = sk_PKCS7_value(asafes, i);
         bagnid = OBJ_obj2nid(p7->type);
         if (bagnid == NID_pkcs7_data) {
             bags = PKCS12_unpack_p7data(p7);
             if (options & INFO)
-                WORKAROUND_printf(bio_err, "PKCS7 Data\n");
+                WORKAROUND_BIO_printf(bio_err, "PKCS7 Data\n");
         } else if (bagnid == NID_pkcs7_encrypted) {
             if (options & INFO) {
-                WORKAROUND_printf(bio_err, "PKCS7 Encrypted data: ");
+                WORKAROUND_BIO_printf(bio_err, "PKCS7 Encrypted data: ");
                 alg_print(bio_err, p7->d.encrypted->enc_data->algorithm);
             }
             bags = PKCS12_unpack_p7encdata(p7, pass, passlen);
@@ -154,6 +179,7 @@ int dump_certs_keys_p12(BIO *out, PKCS12 *p12, char *pass, int passlen, int opti
 int dump_certs_pkeys_bags(BIO *out, STACK_OF(PKCS12_SAFEBAG) *bags,
 	char *pass, int passlen, int options, char *pempass)
 {
+	int num = sk_PKCS12_SAFEBAG_num(bags);
     int i;
     for (i = 0; i < sk_PKCS12_SAFEBAG_num(bags); i++) {
         if (dump_certs_pkeys_bag(out,
@@ -175,7 +201,7 @@ int dump_certs_pkeys_bag(BIO *out, PKCS12_SAFEBAG *bag, char *pass,
     switch (M_PKCS12_bag_type(bag)) {
     case NID_keyBag:
         if (options & INFO)
-            WORKAROUND_printf(bio_err, "Key bag\n");
+            WORKAROUND_BIO_printf(bio_err, "Key bag\n");
         if (options & NOKEYS)
             return 1;
         print_attribs(out, bag->attrib, "Bag Attributes");
@@ -189,7 +215,7 @@ int dump_certs_pkeys_bag(BIO *out, PKCS12_SAFEBAG *bag, char *pass,
 
     case NID_pkcs8ShroudedKeyBag:
         if (options & INFO) {
-            WORKAROUND_printf(bio_err, "Shrouded Keybag: ");
+            WORKAROUND_BIO_printf(bio_err, "Shrouded Keybag: ");
             alg_print(bio_err, bag->value.shkeybag->algor);
         }
         if (options & NOKEYS)
@@ -209,7 +235,7 @@ int dump_certs_pkeys_bag(BIO *out, PKCS12_SAFEBAG *bag, char *pass,
 
     case NID_certBag:
         if (options & INFO)
-            WORKAROUND_printf(bio_err, "Certificate bag\n");
+            WORKAROUND_BIO_printf(bio_err, "Certificate bag\n");
         if (options & NOCERTS)
             return 1;
         if (PKCS12_get_attr(bag, NID_localKeyID)) {
@@ -229,15 +255,15 @@ int dump_certs_pkeys_bag(BIO *out, PKCS12_SAFEBAG *bag, char *pass,
 
     case NID_safeContentsBag:
         if (options & INFO)
-            WORKAROUND_printf(bio_err, "Safe Contents bag\n");
+            WORKAROUND_BIO_printf(bio_err, "Safe Contents bag\n");
         print_attribs(out, bag->attrib, "Bag Attributes");
         return dump_certs_pkeys_bags(out, bag->value.safes, pass,
                                      passlen, options, pempass);
 
     default:
-        WORKAROUND_printf(bio_err, "Warning unsupported bag type: ");
+        WORKAROUND_BIO_printf(bio_err, "Warning unsupported bag type: ");
         i2a_ASN1_OBJECT(bio_err, bag->type);
-        WORKAROUND_printf(bio_err, "\n");
+        WORKAROUND_BIO_printf(bio_err, "\n");
         return 1;
         break;
     }
@@ -253,23 +279,23 @@ int print_attribs(BIO *out, STACK_OF(X509_ATTRIBUTE) *attrlst,
     char *value;
     int i, attr_nid;
     if (!attrlst) {
-        WORKAROUND_printf(out, "%s: <No Attributes>\n", name);
+        WORKAROUND_BIO_printf(out, "%s: <No Attributes>\n", name);
         return 1;
     }
     if (!sk_X509_ATTRIBUTE_num(attrlst)) {
-        WORKAROUND_printf(out, "%s: <Empty Attributes>\n", name);
+        WORKAROUND_BIO_printf(out, "%s: <Empty Attributes>\n", name);
         return 1;
     }
-    WORKAROUND_printf(out, "%s\n", name);
+    WORKAROUND_BIO_printf(out, "%s\n", name);
     for (i = 0; i < sk_X509_ATTRIBUTE_num(attrlst); i++) {
         attr = sk_X509_ATTRIBUTE_value(attrlst, i);
         attr_nid = OBJ_obj2nid(attr->object);
-        WORKAROUND_printf(out, "    ");
+        WORKAROUND_BIO_printf(out, "    ");
         if (attr_nid == NID_undef) {
             i2a_ASN1_OBJECT(out, attr->object);
-            WORKAROUND_printf(out, ": ");
+            WORKAROUND_BIO_printf(out, ": ");
         } else
-            WORKAROUND_printf(out, "%s: ", OBJ_nid2ln(attr_nid));
+            WORKAROUND_BIO_printf(out, "%s: ", OBJ_nid2ln(attr_nid));
 
         if (sk_ASN1_TYPE_num(attr->value.set)) {
             av = sk_ASN1_TYPE_value(attr->value.set, 0);
@@ -277,28 +303,28 @@ int print_attribs(BIO *out, STACK_OF(X509_ATTRIBUTE) *attrlst,
             case V_ASN1_BMPSTRING:
                 value = OPENSSL_uni2asc(av->value.bmpstring->data,
                                         av->value.bmpstring->length);
-                WORKAROUND_printf(out, "%s\n", value);
+                WORKAROUND_BIO_printf(out, "%s\n", value);
                 OPENSSL_free(value);
                 break;
 
             case V_ASN1_OCTET_STRING:
                 hex_prin(out, av->value.octet_string->data,
                          av->value.octet_string->length);
-                WORKAROUND_printf(out, "\n");
+                WORKAROUND_BIO_printf(out, "\n");
                 break;
 
             case V_ASN1_BIT_STRING:
                 hex_prin(out, av->value.bit_string->data,
                          av->value.bit_string->length);
-                WORKAROUND_printf(out, "\n");
+                WORKAROUND_BIO_printf(out, "\n");
                 break;
 
             default:
-                WORKAROUND_printf(out, "<Unsupported tag %d>\n", av->type);
+                WORKAROUND_BIO_printf(out, "<Unsupported tag %d>\n", av->type);
                 break;
             }
         } else
-            WORKAROUND_printf(out, "<No Values>\n");
+            WORKAROUND_BIO_printf(out, "<No Values>\n");
     }
     return 1;
 }
@@ -307,7 +333,7 @@ void hex_prin(BIO *out, unsigned char *buf, int len)
 {
     int i;
     for (i = 0; i < len; i++)
-        WORKAROUND_printf(out, "%02X ", buf[i]);
+        WORKAROUND_BIO_printf(out, "%02X ", buf[i]);
 }
 
 
@@ -324,7 +350,7 @@ int alg_print(BIO *x, X509_ALGOR *alg)
 //
 //	pbenid = OBJ_obj2nid(aoid);
 //
-//	WORKAROUND_printf(x, "%s", OBJ_nid2ln(pbenid));
+//	WORKAROUND_BIO_printf(x, "%s", OBJ_nid2ln(pbenid));
 //
 //	/*
 //	* If PBE algorithm is PBES2 decode algorithm parameters
@@ -336,14 +362,14 @@ int alg_print(BIO *x, X509_ALGOR *alg)
 //		if (aparamtype == V_ASN1_SEQUENCE)
 //			pbe2 = ASN1_item_unpack(aparam, ASN1_ITEM_rptr(PBE2PARAM));
 //		if (pbe2 == NULL) {
-//			WORKAROUND_puts(x, "<unsupported parameters>");
+//			WORKAROUND_BIO_puts(x, "<unsupported parameters>");
 //			goto done;
 //		}
 //		X509_ALGOR_get0(&aoid, &aparamtype, &aparam, pbe2->keyfunc);
 //		pbenid = OBJ_obj2nid(aoid);
 //		X509_ALGOR_get0(&aoid, NULL, NULL, pbe2->encryption);
 //		encnid = OBJ_obj2nid(aoid);
-//		WORKAROUND_printf(x, ", %s, %s", OBJ_nid2ln(pbenid),
+//		WORKAROUND_BIO_printf(x, ", %s, %s", OBJ_nid2ln(pbenid),
 //			OBJ_nid2sn(encnid));
 //		/* If KDF is PBKDF2 decode parameters */
 //		if (pbenid == NID_id_pbkdf2) {
@@ -352,7 +378,7 @@ int alg_print(BIO *x, X509_ALGOR *alg)
 //			if (aparamtype == V_ASN1_SEQUENCE)
 //				kdf = ASN1_item_unpack(aparam, ASN1_ITEM_rptr(PBKDF2PARAM));
 //			if (kdf == NULL) {
-//				WORKAROUND_puts(x, "<unsupported parameters>");
+//				WORKAROUND_BIO_puts(x, "<unsupported parameters>");
 //				goto done;
 //			}
 //
@@ -363,7 +389,7 @@ int alg_print(BIO *x, X509_ALGOR *alg)
 //				X509_ALGOR_get0(&aoid, NULL, NULL, kdf->prf);
 //				prfnid = OBJ_obj2nid(aoid);
 //			}
-//			WORKAROUND_printf(x, ", Iteration %ld, PRF %s",
+//			WORKAROUND_BIO_printf(x, ", Iteration %ld, PRF %s",
 //				ASN1_INTEGER_get(kdf->iter), OBJ_nid2sn(prfnid));
 //			PBKDF2PARAM_free(kdf);
 //		}
@@ -373,14 +399,14 @@ int alg_print(BIO *x, X509_ALGOR *alg)
 //		if (aparamtype == V_ASN1_SEQUENCE)
 //			pbe = ASN1_item_unpack(aparam, ASN1_ITEM_rptr(PBEPARAM));
 //		if (pbe == NULL) {
-//			WORKAROUND_puts(x, "<unsupported parameters>");
+//			WORKAROUND_BIO_puts(x, "<unsupported parameters>");
 //			goto done;
 //		}
-//		WORKAROUND_printf(x, ", Iteration %ld", ASN1_INTEGER_get(pbe->iter));
+//		WORKAROUND_BIO_printf(x, ", Iteration %ld", ASN1_INTEGER_get(pbe->iter));
 //		PBEPARAM_free(pbe);
 //	}
 //done:
-//	WORKAROUND_puts(x, "\n");
+//	WORKAROUND_BIO_puts(x, "\n");
 //	return 1;
 //}
 
@@ -389,15 +415,36 @@ int dump_cert_text(BIO *out, X509 *x)
 	char *p;
 
 	p = X509_NAME_oneline(X509_get_subject_name(x), NULL, 0);
-	WORKAROUND_puts(out, "subject=");
-	WORKAROUND_puts(out, p);
+	WORKAROUND_BIO_puts(out, "subject=");
+	WORKAROUND_BIO_puts(out, p);
 	OPENSSL_free(p);
 
 	p = X509_NAME_oneline(X509_get_issuer_name(x), NULL, 0);
-	WORKAROUND_puts(out, "\nissuer=");
-	WORKAROUND_puts(out, p);
-	WORKAROUND_puts(out, "\n");
+	WORKAROUND_BIO_puts(out, "\nissuer=");
+	WORKAROUND_BIO_puts(out, p);
+	WORKAROUND_BIO_puts(out, "\n");
 	OPENSSL_free(p);
+
+	int self_signed = X509_verify(x, X509_get_pubkey(x));
+	WORKAROUND_BIO_printf(out, "Self Signed: %s\n ", self_signed ? "True" : "False");
+
+	// Finger print
+	int j;
+	unsigned int n;
+	unsigned char md[EVP_MAX_MD_SIZE];
+	const EVP_MD *fdig = EVP_sha256();
+
+	if (!X509_digest(x, fdig, md, &n)) {
+		WORKAROUND_BIO_printf(bio_err, "out of memory\n");
+		return 1;
+	}
+	WORKAROUND_BIO_printf(out, "%s Fingerprint=",
+		OBJ_nid2sn(EVP_MD_type(fdig)));
+	for (j = 0; j < (int)n; j++) {
+		WORKAROUND_BIO_printf(out, "%02X%c", md[j], (j + 1 == (int)n)
+			? '\n' : ':');
+	}
+
 
 	return 0;
 }
